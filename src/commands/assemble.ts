a38a523c1ab61ea31cf9f0a5a95fca66d1d037f3
m-tahaali <tahaali.6074+github@gmail.com>
2024-08-24 12:03:57 +0500

    Fix Long Names and Spaces in Names not working with DOSBOX Fixes #5

--- src/commands/assemble.ts
+++ src/commands/assemble.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 import { existsSync } from 'fs';
 import { join } from 'path';
 
-export async function assemble() {
+export async function assemble(outputFileBaseNameWithoutExt?: string) {
     const editor = vscode.window.activeTextEditor;
     if (!editor) {
         vscode.window.showErrorMessage('No active text editor found');
@@ -23,16 +23,17 @@ export async function assemble() {
     const configs = vscode.workspace.getConfiguration('nasm-tools');
     const nasmCommand = configs.get('nasmCommand');
 
+    const fileDir = document.fileName.split('\\').slice(0, -1).join('\\');
     const fileNameWithoutExt = document.fileName.split('.').slice(0, -1).join('.');
-    const fileDir = fileNameWithoutExt.split('\\').slice(0, -1).join('\\');
+    let outputFileNameWithoutExt = outputFileBaseNameWithoutExt ? fileDir + "\\" + outputFileBaseNameWithoutExt : fileNameWithoutExt;
     let terminal = vscode.window.createTerminal('NASM Cleanup');
     terminal.sendText(`cd "${fileDir}"`);
     // Delete Previous Files
-    if (existsSync(join(fileNameWithoutExt + ".com"))) {
-        terminal.sendText(`del "${fileNameWithoutExt}.com"`);
+    if (existsSync(join(outputFileNameWithoutExt + ".com"))) {
+        terminal.sendText(`del "${outputFileNameWithoutExt}.com"`);
     }
-    if (existsSync(join(fileNameWithoutExt + ".lst"))) {
-        terminal.sendText(`del "${fileNameWithoutExt}.lst"`);
+    if (existsSync(join(outputFileNameWithoutExt + ".lst"))) {
+        terminal.sendText(`del "${outputFileNameWithoutExt}.lst"`);
     }
     terminal.sendText('exit');
     while (terminal.exitStatus === undefined) {
@@ -40,18 +41,18 @@ export async function assemble() {
     }
     terminal = vscode.window.createTerminal('NASM Assemble', nasmCommand as string, [
         `${document.fileName}`,
-        "-o", `${fileNameWithoutExt}.com`,
-        "-l", `${fileNameWithoutExt}.lst`
+        "-o", `${outputFileNameWithoutExt}.com`,
+        "-l", `${outputFileNameWithoutExt}.lst`
     ]);
     while (terminal.exitStatus === undefined) {
         await new Promise(resolve => setTimeout(resolve, 500));
     }
-    if (existsSync(join(fileNameWithoutExt + ".com"))) {
+    if (existsSync(join(outputFileNameWithoutExt + ".com"))) {
         vscode.window.showInformationMessage('Assembly Successful');
         return true;
     } else {
         vscode.window.showErrorMessage('Assembly Failed, See LST File for Details');
-        vscode.workspace.openTextDocument(join(fileNameWithoutExt + ".lst")).then(doc => {
+        vscode.workspace.openTextDocument(join(outputFileNameWithoutExt + ".lst")).then(doc => {
             vscode.window.showTextDocument(doc);
         });
         return false;
